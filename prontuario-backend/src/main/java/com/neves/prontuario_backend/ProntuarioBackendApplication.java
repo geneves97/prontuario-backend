1602f2a92bfcd3918edb62298f05e9fa6d0f70e7
George Neves <georgeneves.dev@gmail.com>
2024-09-02 10:50:05 -0300

    initial commit

---
+++ prontuario-backend/src/main/java/com/neves/prontuario_backend/ProntuarioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neves.prontuario_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntuarioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntuarioBackendApplication.class, args);
+	}
+
+}
