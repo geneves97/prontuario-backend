433fc731d23a812d05e885ecac43de524c8aefc1
GeorgeNeves <georgeneves.dev@gmail.com>
2025-04-07 22:07:44 -0300

    feat #6: Create EmpregadoEntity empty.

--- src/main/java/com/neves/prontuario_backend/entity/EmpregadoEntity.java
+++ src/main/java/com/neves/prontuario_backend/entity/EmpregadoEntity.java
@@ -1,4 +1,37 @@
 package com.neves.prontuario_backend.entity;
 
+import jakarta.persistence.*;
+
 public class EmpregadoEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @SequenceGenerator(name="id")
+    @Column(name = "ID")
+    Integer id;
+
+    @Column(name="nome")
+    String nome;
+
+    @Column(name="email")
+    String email;
+
+    @Column(name="cargo")
+    String cargo;
+
+    public void visualizarProntuario (){
+        return ;
+    }
+
+    public void visualizarReceituario (){
+        return ;
+    }
+
+    public void criarProntuario (){
+        return ;
+    }
+
+    public void editarPaciente (){
+        return ;
+    }
 }
