4a6040060e341df7c7cd211e0e016cc7eb016e43
GeorgeNeves <georgeneves.dev@gmail.com>
2025-04-10 22:05:05 -0300

    feat #6: Mapping MedicamentoEntity

--- src/main/java/com/neves/prontuario_backend/entity/MedicamentoEntity.java
+++ src/main/java/com/neves/prontuario_backend/entity/MedicamentoEntity.java
@@ -1,4 +1,25 @@
 package com.neves.prontuario_backend.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class MedicamentoEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @SequenceGenerator(name="id")
+    @Column(name = "ID")
+    Integer id;
+
+    @Column(name = "nome")
+    String nome;
+
+    @Column(name = "dose")
+    Float dose;
+
+
 }
